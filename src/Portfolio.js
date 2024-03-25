cf434945600a18e20ee8d09b08f6420f6e6e55c7
Burhan <m.burhanmoin1@gmail.com>
2024-03-25 08:36:47 +0500

    'morningcheck'

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -112,19 +112,20 @@ const PortfolioItem = ({ image, name, description }) => {
 const Portfolio = () => {
   // Sample data for portfolio items
   const portfolioData = [
-    { id: 1, image: CasgrainImage, name: '7530 - 7538 Casgrain', description: 'Apartment - Montreal' },
     { id: 2, image: Frontenac, name: '2087 - 2093 Frontenac', description: 'Apartment - Montreal' },
-    { id: 3, image: VillImage, name: '294 Villienueve Quest', description: 'Apartment - Montreal' },
-    { id: 4, image: Fullum, name: '2300 - 2551 Fullum', description: 'Apartment - Montreal' },
-    { id: 5, image: Sherbrooke, name: '2290 Sherbrooke', description: 'Apartment - Montreal'},
-    { id: 10, image: Sherbrooketwo, name: '2308 Sherbrooke', description: 'Apartment - Montreal'},
-    { id: 10, image: Sherbrookefour, name: '2306 Sherbrooke', description: 'Apartment - Montreal'},
-    { id: 10, image: Sherbrookethree, name: '2314 Sherbrooke', description: 'Apartment - Montreal'},
-    { id: 10, image: StJacques, name: '7388 St Jacques', description: 'Apartment - Montreal'},
     { id: 6, image: MarieAnne, name: '2189 - 2199 Marie-Anne', description: 'Apartment - Montreal'},
+    { id: 5, image: Sherbrooke, name: '2290 Sherbrooke', description: 'Apartment - Montreal'},
+    { id: 4, image: Fullum, name: '2300 - 2551 Fullum', description: 'Apartment - Montreal' },
+    { id: 10, image: Sherbrookefour, name: '2306 Sherbrooke', description: 'Apartment - Montreal'},
+    { id: 10, image: Sherbrooketwo, name: '2308 Sherbrooke', description: 'Apartment - Montreal'},
+    { id: 10, image: Sherbrookethree, name: '2314 Sherbrooke', description: 'Apartment - Montreal'},
     { id: 7, image: Dandurand, name: '2700 - 2710 Dandurand', description: 'Apartment - Montreal'},
-    { id: 8, image: Hochelaga8578, name: '8578 - 8590 Baldwin', description: 'Apartment - Montreal'},
+    { id: 3, image: VillImage, name: '294 Villienueve Quest', description: 'Apartment - Montreal' },
     { id: 8, image: Hochelaga3657, name: '3657 - 3667 De Chambly', description: 'Apartment - Montreal'},
+    { id: 10, image: StJacques, name: '7388 St Jacques', description: 'Apartment - Montreal'},
+    { id: 1, image: CasgrainImage, name: '7530 - 7538 Casgrain', description: 'Apartment - Montreal' },
+    { id: 8, image: Hochelaga8578, name: '8578 - 8590 Baldwin', description: 'Apartment - Montreal'},
+    
   ];
 
     useEffect(() => {
